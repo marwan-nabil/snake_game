7d869f6eb665028e18e4c07c1f6073ff0ba84e48
marwan-nabil <marwan.nabil.mohammed@gmail.com>
2020-06-22 08:39:37 +0200

    refactored code into 2 distinct layers (game & platform) in preparation for further refactoring into a separate platform exe calling into a game dll file when dll loading code is done.

--- game.h
+++ game.h
@@ -1,10 +1,32 @@
 #pragma once
+#include <list>
 #include "basic_defs.h"
-#include "snake.h"
 
+#define BLOCK_SIZE_PIXELS 25.0f
 #define MOVING_PERIOD 0.2
 #define RESTART_TIME 1.0
 
+struct color {
+    u8 Red, Green, Blue;
+};
+
+struct block {
+    i32 X, Y;
+};
+
+enum key {
+    KeyUp, KeyDown, KeyLeft, KeyRight, KeyEscape
+};
+
+enum direction {
+    Up, Down, Left, Right, NoChange
+};
+
+struct snake {
+    direction CurrentDirection;
+    std::list<block> Body;
+    block Tail;
+};
 
 struct snake_game {
     snake Snake;
@@ -15,10 +37,22 @@ struct snake_game {
     f64 WaitingTime;
 };
 
-enum key {
-    KeyUp, KeyDown, KeyLeft, KeyRight, KeyEscape
-};
 
+snake NewSnake(i32 X, i32 Y);
+
+void DrawSnake(snake *Snake);
+
+void GetSnakeHeadPosition(snake *Snake, i32 *X, i32 *Y);
+
+direction GetSnakeHeadDirection(snake *Snake);
+
+void MoveSnakeForward(snake *Snake, direction Direction);
+
+void GetNextSnakeHeadPosition(snake *Snake, direction Direction, i32 *X, i32 *Y);
+
+void RestoreSnakeTail(snake *Snake);
+
+bool IsTailOverlapping(snake *Snake, i32 X, i32 Y);
 direction GetOppositeDirection(direction Dir);
 
 snake_game NewGame(i32 Width, i32 Height);
@@ -38,3 +72,11 @@ bool CheckIfSnakeAlive(snake_game *Game, direction Direction);
 void AddFood(snake_game *Game);
 
 void RestartGame(snake_game *Game);
+
+f64 GameCoordsToPixelCoords(i32 GameCoords);
+
+u32 GameCoordsToPixelCoordsU32(i32 GameCoords);
+
+void DrawBlock(color Color, i32 X, i32 Y);
+
+void DrawRectangle(color Color, i32 X, i32 Y, i32 Width, i32 Height);
--- platform.h
+++ platform.h
@@ -2,11 +2,6 @@
 #include <Windows.h>
 #include "basic_defs.h"
 
-
-struct color {
-    u8 Red, Green, Blue;
-};
-
 struct WindowDimension {
     i32 Width;
     i32 Height;
@@ -20,14 +15,5 @@ struct offscreen_buffer {
     i32 BytesPerPixel;
     i32 Pitch;  // bytes per row
 };
-
-extern f32 GameUpdateHz;
-extern offscreen_buffer GlobalBackBuffer;
-
-void ResizeBackBuffer(offscreen_buffer *Buffer, i32 Width, i32 Height);
-
-void UpdateWindowFromBuffer(HDC DevContext, i32 WindowWidth, i32 WindowHeight, offscreen_buffer *Buffer);
-
-WindowDimension GetWindowDimension(HWND Window);
 
 void RenderRectangle(color Color, f64 X, f64 Y, f64 Width, f64 Height);
